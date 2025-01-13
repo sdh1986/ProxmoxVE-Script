2ffc029e65860f8fd56845920462df193a2445e7
sparke <31507699+sdh1986@users.noreply.github.com>
2025-01-13 10:37:12 +0800

    Update Pve.sh

--- Pve.sh
+++ Pve.sh
@@ -141,10 +141,10 @@ Main() {
   UpdateAndUpgrade
 
   InstallPackage "openvswitch-switch"
+  LogInfo "InstallPackage."
 
   ReplaceSources "/usr/share/perl5/PVE/APLInfo.pm" "http://download.proxmox.com" "https://mirrors.ustc.edu.cn/proxmox"
-
-  sleep 15
+  LogInfo "ReplaceSources."
 
   systemctl restart pveproxy.service pvedaemon.service
   LogInfo "Relevant services restarted."
